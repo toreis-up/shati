790b31d963f917bcebb3c29993462a75ab3aa4bc
toreis-up <reisan03@reisan.dev>
2025-06-29 00:40:46 +0900

    chore(style): move stylesheet to assets

--- apps/ShaTi-frontend/nuxt.config.ts
+++ apps/ShaTi-frontend/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: 'src/',
   modules: ['@nuxt/ui', '@pinia/nuxt'],
-  css: ['~/assets/css/main.css'],
+  css: ['assets/css/main.css'],
   runtimeConfig: {
     public: {
       apiBase: 'http://127.0.0.1:8787',
