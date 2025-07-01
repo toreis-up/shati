a283ae42853bd4c90e2b4528f24a1b5d4a3b870d
toreis-up <reisan03@reisan.dev>
2025-07-01 22:25:21 +0900

    refactor: rename file path

--- apps/ShaTi-frontend/src/components/Header.vue
+++ apps/ShaTi-frontend/src/components/Header.vue
@@ -2,14 +2,14 @@
   <header class="p-4 border-b border-primary-300">
     <div class="flex justify-between items-center max-w-[1440px] mx-auto">
       <NuxtLink to="/" class="flex items-center gap-2" aria-label="Home">
-        <img src="~/assets/img/shati.svg" alt="ShaTi Logo" class="h-10 w-10" />
+        <img src="~/assets/images/shati.svg" alt="ShaTi Logo" class="h-10 w-10" />
         <span class="text-xl font-bold">ShaTi</span>
       </NuxtLink>
       <nav>
         <ul class="flex items-center space-x-4">
           <li>
             <a href="#" class="hover:text-gray-400">
-              <img src="~/assets/img/folder-clock-outline.svg" alt="History" class="h-8 w-8" />
+              <img src="~/assets/images/folder-clock-outline.svg" alt="History" class="h-8 w-8" />
             </a>
           </li>
         </ul>
