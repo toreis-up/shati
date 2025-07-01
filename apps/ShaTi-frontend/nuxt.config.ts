db8e8aba1d9e524af8d37eec1deba7f7cabc6053
toreis-up <reisan03@reisan.dev>
2025-07-01 22:23:59 +0900

    chore: configure @nuxt/image

--- apps/ShaTi-frontend/nuxt.config.ts
+++ apps/ShaTi-frontend/nuxt.config.ts
@@ -6,7 +6,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   srcDir: 'src/',
-  modules: ['@nuxt/ui', '@pinia/nuxt'],
+  modules: ['@nuxt/ui', '@pinia/nuxt', '@nuxt/image'],
+  image: {
+    format: ['webp'],
+  },
   css: ['assets/css/main.css'],
   runtimeConfig: {
     public: {
