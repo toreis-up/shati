652030f0c1a57da526065d87baf16e92c690c083
toreis-up <reisan03@reisan.dev>
2025-09-29 23:23:59 +0900

    feat: add assets for white
    style: change

--- apps/ShaTi-frontend/app/components/ui/textInput.vue
+++ apps/ShaTi-frontend/app/components/ui/textInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="disabled ? '' : 'hover:bg-primary-300'" class="my-2 inline-flex relative text-4xl font-bold transition duration-200 rounded-sm justify-center">
     <div class="inline-flex relative overflow-hidden min-w-4 px-5 py-5 opacity-0 whitespace-pre before:content-[''] empty:before:content-[attr(data-placeholder)]" :data-placeholder="placeholder">{{ modelValue }}</div>
-    <input type="text" class="absolute top-0 left-0 box-border w-full px-5 py-4 focus:outline-none underline decoration-transparent underline-offset-4 decoration-2 focus-visible:decoration-slate-200 transition-[text-decoration-color] duration-200 whitespace-pre" @input="onInput" :disabled="disabled" :placeholder="placeholder" :value="modelValue" />
+    <input type="text" class="absolute top-0 left-0 box-border w-full px-5 py-4 focus:outline-none underline decoration-transparent underline-offset-4 decoration-2 focus-visible:decoration-neutral-800 transition-[text-decoration-color] duration-200 whitespace-pre" @input="onInput" :disabled="disabled" :placeholder="placeholder" :value="modelValue" />
   </div>
 </template>
 
