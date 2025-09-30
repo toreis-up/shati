150e5b2502206efba2f50b60360a44f8deb02f8b
とれいす <65478744+toreis-up@users.noreply.github.com>
2025-09-30 17:11:46 +0900

    Support light theme officially (#48)
    * feat: add assets for white
    
    style: change
    
    * feat: add logo
    
    feat: asset change
    
    * fix: using skeleton

---
+++ apps/ShaTi-frontend/app/components/ui/FooterLogo.vue
@@ -0,0 +1,16 @@
+<template>
+  <USkeleton v-if="!isMounted" class="rounded-full" />
+  <NuxtImg
+    v-else-if="colorMode === 'dark'"
+    src="/images/shati_lp_white.webp"
+    alt="ShaTi Logo"
+  />
+  <NuxtImg v-else src="/images/shati_lp_color.webp" alt="ShaTi Logo" />
+</template>
+
+<script lang="ts" setup>
+import { useColorMode, useMounted } from '@vueuse/core';
+
+const colorMode = useColorMode();
+const isMounted = useMounted();
+</script>
---
+++ apps/ShaTi-frontend/app/components/ui/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <USkeleton
+      v-if="!isMounted"
+      class="w-full h-full"
+      />
+    <NuxtImg v-else-if="colorMode === 'dark'" src="/images/shati_white.svg" alt="ShaTi Logo" />
+    <NuxtImg v-else src="/images/shati_black.svg" alt="ShaTi Logo" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useColorMode, useMounted } from '@vueuse/core';
+
+const colorMode = useColorMode();
+const isMounted = useMounted();
+</script>
